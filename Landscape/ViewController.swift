e53d0654936fa8f8f95639da14f9b006cbb769a3
KO <kj.kozk@gmail.com>
2017-01-04 22:19:15 +0900

    Initial Commit

---
+++ Landscape/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Landscape
+//
+//  Created by KO on 2017/01/04.
+//  Copyright © 2017年 KO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
